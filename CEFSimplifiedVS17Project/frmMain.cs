491dec085f20dbf05da4555fdf742d74e049a914
MRAsoft <info.mrasoft@gmail.com>
2021-02-21 10:35:48 +0100

    Initial Commit

---
+++ CEFSimplifiedVS17Project/frmMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CefSharp;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+
+namespace CEFSimplifiedVS17Project
+{
+    public partial class frmMain : Form
+    {
+        private static CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser;
+        private const string Url = "https://www.google.com/";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // this.WindowState = FormWindowState.Maximized;
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+
+            // EnableHighDPISupport
+            Cef.EnableHighDPISupport();
+
+            chromiumWebBrowser = new CefSharp.WinForms.ChromiumWebBrowser(Url);
+            this.panel1.Controls.Add(chromiumWebBrowser);
+            chromiumWebBrowser.Dock = DockStyle.Fill;
+
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
